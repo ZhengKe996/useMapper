166694e0f923e314d58bff3c15c70a0e6bdc4502
Zhengke <zhengke0509@foxmail.com>
2021-10-23 18:53:39 +0800

    ok

--- useGetters.js
+++ useGetters.js
@@ -1,10 +1,12 @@
 import { mapGetters, createNamespacedHelpers } from 'vuex'
 import { useMapper } from './useMapper'
 
-export function useState (mapper) {
+export function useState (moduleName, mapper) {
     let mapperFunction = mapGetters
     if (typeof moduleName === 'string' && moduleName.length > 0) {
         mapperFunction = createNamespacedHelpers(moduleName).mapGetters
+    } else {
+        mapper = moduleName
     }
     return useMapper(mapper, mapperFunction)
 }
--- useState.js
+++ useState.js
@@ -5,6 +5,8 @@ export function useState (moduleName, mapper) {
     let mapperFunction = mapState
     if (typeof moduleName === 'string' && moduleName.length > 0) {
         mapperFunction = createNamespacedHelpers(moduleName).mapState
+    } else {
+        mapper = moduleName
     }
 
     return useMapper(mapper, mapperFunction)
